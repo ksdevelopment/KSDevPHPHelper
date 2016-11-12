34e8aa33847edb4d46e046e113dbece474684d9d
Kasper Brøgger Svendsen KS Development ApS <ks@ksvendsen.dk>
2016-11-12 20:24:11 +0100

    added filesize2str

--- src/number-utils.php
+++ src/number-utils.php
@@ -11,3 +11,13 @@
 function between($val, $min, $max) {
     return ($val >= $min && $val <= $max);
 }
+
+if(!function_exists('filesize2str')) {
+    function filesize2str($size, $precision = 2)
+    {
+        $base = log($size, 1024);
+        $suffix = getKey(floor($base), ['', 'kb', 'mb', 'gb', 'tb', 'pb'], '');
+
+        return str_replace('.',',', round(pow(1024, $base - floor($base)), $precision) .' '. $suffix);
+    }
+}
